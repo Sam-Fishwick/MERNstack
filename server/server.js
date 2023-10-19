70b120e2df5df9f9beccbfad5eedd5ef826be264
Sam Fishwick <sam.fishwick@hotmail.com>
2023-10-19 17:58:33 +0100

    Progress

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-// import os/dotenv??
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const db = require('./db');
+const movieRouter = require('./routes/movie-router');
+
+const app = express();
 
 const PORT = 5000 //parseInt(process.env.PORT);
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to my MERN stack app!')
+});
+
+app.use('/api', movieRouter);
 
 app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`);
